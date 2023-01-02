60cfab7e35f09fcbc2f39c17ee53c81dc31aea4d
Govardhan <govardhanduddela522@gmail.com>
2023-01-02 14:27:28 +0530

    commit

---
+++ src/com/example/pounds/PoundsClass.java
@@ -0,0 +1,10 @@
+package com.example.pounds;
+
+public class PoundsClass {
+    public static void main(String[] args) {
+        double value=2.20463d;
+        double pound=value*0.453592;
+        System.out.println(pound);
+
+    }
+}
